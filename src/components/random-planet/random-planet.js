a8285d698a67abed9c54504de411bfcf0182f075
Rustam Qurbanov <rustam.qurbanov02@gmail.com>
2024-03-10 21:05:32 +0400

    refactoring random-planet and swapi-service

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -8,11 +8,7 @@ export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null,
+    planet: {},
   };
 
   // constructor() {
@@ -25,21 +21,19 @@ export default class RandomPlanet extends Component {
     document.title = `You clicked ${this.state.count} times`;
   }
 
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  };
+
   updatePlanet() {
     const id = Math.floor(Math.random() * 19 + 1);
-    this.swapiService.getPlanet(id).then((planet) => {
-      this.setState({
-        id,
-        name: planet.name,
-        population: planet.population,
-        rotationPeriod: planet.rotation_period,
-        diameter: planet.diameter,
-      });
-    });
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
   render() {
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const {
+      planet: { id, name, population, rotationPeriod, diameter },
+    } = this.state;
 
     return (
       <div className="random-planet jumbotron rounded bg-dark">
