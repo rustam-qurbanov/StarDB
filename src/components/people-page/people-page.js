945a16b7a057586f42795f38c1ef00b375fc1762
Rustam Qurbanov <rustam.qurbanov02@gmail.com>
2024-03-15 16:44:12 +0400

    add component error-boundry

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 import SwapiService from "../../services/swapi-service";
 import ItemList from "../item-list/item-list";
 import PersonDetails from "../person-details/person-details";
-import ErrorIndicator from "../error-indicator/error-indicator";
-import Row from '../row'
+
+import Row from "../row";
+import ErrorBoundry from "../error-boundry";
 
 import "./people-page.css";
 
@@ -13,34 +14,26 @@ export default class PeoplePage extends Component {
 
   state = {
     selectedPerson: 3,
-    hasError: false,
   };
-
-  componentDidCatch(error, info) {
-    this.setState({
-      hasError: true,
-    });
-  }
 
   onPersonSelected = (selectedPerson) => {
     this.setState({ selectedPerson });
   };
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
-
     const itemList = (
       <ItemList
         onItemSelected={this.onPersonSelected}
         getData={this.swapiService.getAllPeople}
-        renderItem={({ name, birthYear }) => `${name} - ( ${birthYear} )`}
-      />
+      >
+        {(i) => `${i.name} - ( ${i.birthYear} )`}
+      </ItemList>
     );
 
     const personDetails = (
-      <PersonDetails personId={this.state.selectedPerson} />
+      <ErrorBoundry>
+        <PersonDetails personId={this.state.selectedPerson} />
+      </ErrorBoundry>
     );
 
     return (
